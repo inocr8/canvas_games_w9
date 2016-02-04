287ca254d95114373c6d33a1a629caae130e63a7
Euan Walls <apps@inocr8.co>
2016-02-04 15:55:51 +0000

    diagonal buttons added

--- public/app.js
+++ public/app.js
@@ -6,7 +6,11 @@ window.onload = function(){
   var leftButton = document.getElementById('button-left');
   var rightButton = document.getElementById('button-right');
   var upButton = document.getElementById('button-up');
+  var upLeftButton = document.getElementById('button-up-left');
+  var upRightButton = document.getElementById('button-up-right');
   var downButton = document.getElementById('button-down');
+  var downLeftButton = document.getElementById('button-down-left');
+  var downRightButton = document.getElementById('button-down-right');
   var resetButton = document.getElementById('reset');
   var dropdown = document.getElementById('colourList');
   var numberForm = document.getElementById('numberForm');
@@ -41,10 +45,42 @@ window.onload = function(){
     yPosition -= sketchIncrement;
   };
 
+  upLeftButton.onclick=function(){
+    console.log('UpLeft');
+    context.lineTo(xPosition - sketchIncrement, yPosition - sketchIncrement);
+    context.stroke();
+    xPosition -= sketchIncrement;
+    yPosition -= sketchIncrement;
+  };
+
+  upRightButton.onclick=function(){
+    console.log('UpRight');
+    context.lineTo(xPosition + sketchIncrement, yPosition - sketchIncrement);
+    context.stroke();
+    xPosition += sketchIncrement;
+    yPosition -= sketchIncrement;
+  };
+
   downButton.onclick=function(){
     console.log('Down');
     context.lineTo(xPosition, yPosition + sketchIncrement);
     context.stroke();
+    yPosition += sketchIncrement;
+  };
+
+  downLeftButton.onclick=function(){
+    console.log('DownLeft');
+    context.lineTo(xPosition - sketchIncrement, yPosition + sketchIncrement);
+    context.stroke();
+    xPosition -= sketchIncrement;
+    yPosition += sketchIncrement;
+  };
+
+  downRightButton.onclick=function(){
+    console.log('DownRight');
+    context.lineTo(xPosition + sketchIncrement, yPosition + sketchIncrement);
+    context.stroke();
+    xPosition += sketchIncrement;
     yPosition += sketchIncrement;
   };
 
@@ -54,20 +90,20 @@ window.onload = function(){
     xPosition = 50;
     yPosition = 50;
     context.beginPath();
-  }
+  };
 
   dropdown.onchange = function(){
     var picked = this.value;
     console.log(picked);
     context.beginPath();
     context.strokeStyle = picked;
-  }
+  };
 
   numberForm.onchange = function(){
     var picked = this.quantity.value;
     console.log(picked);
     context.lineWidth = picked;
     context.beginPath();
-  }
+  };
 
 };
